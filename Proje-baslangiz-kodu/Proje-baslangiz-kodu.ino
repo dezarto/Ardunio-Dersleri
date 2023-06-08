5e1d424dff3671e445135cc77af40dbee0a1eaa3
dezarto <yaman_semir@hotmail.com>
2023-06-08 19:43:32 +0300

    Create Proje-baslangiz-kodu.ino

---
+++ Proje-baslangiz-kodu/Proje-baslangiz-kodu.ino
@@ -0,0 +1,8 @@
+void setup() {
+  Serial.begin(9600);   // Seri haberleşme kodu
+  Serial.println("Hello World\n"); // Seri port ekranına metin yazdırır. 
+                                    // \n veya println yeni satıra yazdırır
+}
+
+void loop() {
+}
