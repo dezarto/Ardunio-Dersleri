a78354b249ed4a48235b72a2457ee148242eab2c
dezarto <yaman_semir@hotmail.com>
2023-06-09 09:09:12 +0300

    update 09.06.2023

--- Led-Kontrolu/Led-Kontrolu.ino
+++ Led-Kontrolu/Led-Kontrolu.ino
@@ -1,10 +1,17 @@
+/*
+Bu kod satırında 4 farklı led 1 saniye aralıklarla sönüp yanmaktadır.
+Tarih: 09.06.2023
+Yazar: dezarto
+*/
+
 int led1 = 2;
 int led2 = 3;
 int led3 = 4;
 int led4 = 5;
+int defaultLed = 13;
 
 void setup() {
-  pinMode(13, OUTPUT);
+  pinMode(defaultLed, OUTPUT);  // default led
 
   pinMode(2, led1);  // tek tek girişler tanımlanır
   pinMode(3, led2);
@@ -13,17 +20,21 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(5, led1);   // ledlerin yanmasını sağlar
+  digitalWrite(defaultLed, HIGH);
+
+  digitalWrite(5, led1);  // ledlerin yanmasını sağlar
   digitalWrite(2, led2);
   digitalWrite(3, led3);
   digitalWrite(4, led4);
-  digitalWrite(13, HIGH);
+
   delay(1000);  // 1 saniye bekletir
+
+  digitalWrite(13, LOW);
 
   digitalWrite(5, LOW);  // ledlerin sönmesini sağlar
   digitalWrite(2, LOW);
   digitalWrite(3, LOW);
   digitalWrite(4, LOW);
-  digitalWrite(13, LOW);
+
   delay(1000);
 }
