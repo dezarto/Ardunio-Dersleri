dd6efb4aae177fef2c748a8b21c7c6c92a0b0761
dezarto <yaman_semir@hotmail.com>
2023-06-09 09:01:48 +0300

    update 09.06.2023

---
+++ Proje Baslangiz Kodu/Proje-baslangic-kod.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);               // Seri haberleşme kodu
+  Serial.println("Hello World\n");  // Seri port ekranına metin yazdırır.
+                                    // "\n" veya "println" yeni satıra yazdırır
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
